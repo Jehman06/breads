7275ffaf9cb9950d638ab5d6b8d7f4e37fee1e4d
Jeremy Lehmann <jeremylehmann@Jeremys-MacBook-Pro.local>
2023-03-14 21:16:13 -0700

    Create and New routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -3,11 +3,16 @@ const express = require('express')
 const breads_router = express.Router()
 const Bread = require('../models/bread.js')
 
+// NEW ROUTE
+breads_router.get('/new', (req, res) => {
+    res.render('new')
+})
+
 // SHOW ROUTE
 breads_router.get('/:arrayIndex', (req, res) => {
     if (Bread[req.params.arrayIndex]) {
         res.render('Show', {
-            bread: Bread[req.params.arrayIndex]
+            bread:Bread[req.params.arrayIndex]
         })
     } else {
         res.send('Whoopsie, page not found.')
@@ -16,12 +21,26 @@ breads_router.get('/:arrayIndex', (req, res) => {
 
 // INDEX
 breads_router.get('/', (req, res) => {
-    res.render('Show', 
+    res.render('Index', 
     {
         breads: Bread,
         title: 'Index Page'
     })
     // res.send(Bread)
+})
+
+// CREATE ROUTE
+breads_router.post('/', (req, res) => {
+    if(!req.body.image) {
+        req.body.image = "https://www.seriouseats.com/thmb/gBMNe_J1QqbAz_QAXfW7-bRrhnw=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/better-no-knead-bread-recipe-hero-01_1-48d654bfadeb4a5caf9b233b00fc74ca.JPG"
+    }
+    if(req.body.hasGluten === 'on') {
+        req.body.hasGluten = true
+    } else {
+        req.body.hasGluten = false
+    }
+    Bread.push(req.body)
+    res.redirect('/breads')
 })
 
 module.exports = breads_router
--- views/show.jsx
+++ views/show.jsx
@@ -2,20 +2,21 @@
 const React = require('react')
 const Default = require('./layouts/Default')
 
-function Show({bread}) {
+function Show ({bread}) {
     return (
         <Default>
+            <h2>Show Page</h2>
             <h3>{bread.name}</h3>
             <p>
-                and it 
+                and it
                 {
-                    bread.hasGluten 
+                    bread.hasGluten
                     ? <span> does </span>
                     : <span> does NOT </span>
                 }
-                have gluten 
+                have gluten.
             </p>
-            <img src={bread.image} alt={bread.name}></img>
+            <img src={bread.image} alt={bread.name} />
             <li><a href="/breads">Go home</a></li>
         </Default>
     )
